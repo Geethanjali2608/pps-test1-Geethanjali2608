a01ace8903446e2475b9fbf1fc941f5604ac46fb
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-01-26 07:28:40 +0000

    cmp value by ref

--- p5original.c
+++ p5original.c
@@ -1,2 +1,33 @@
 #include<stdio.h>
-int input(int *a,int *b,int *c)
+void input(int *a,int *b,int *c)
+{
+  printf("enter the number:\n");
+  scanf("%d %d %d",a,b,c);
+}
+int cmp(int a,int b,int c,int *large)
+{
+  if(a>b && a>c)
+  {
+    *large=a;
+  }  
+  else if(b>c)
+  {
+   *large=b;
+  } 
+  else
+  {
+   *large=c;
+  } 
+}
+void output(int large)
+{
+  printf("largest number is %d\n",large);
+}
+int main()
+{
+  int a,b,c,large;
+  input(&a,&b,&c);
+  cmp(a,b,c,&large);
+  output(large);
+  return 0;
+}  
