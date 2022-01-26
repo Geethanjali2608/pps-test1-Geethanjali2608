b14b48c8c2ec6a894641f97c9b3dafa4dd91326d
Geethanjali2608 <96220572+Geethanjali2608@users.noreply.github.com>
2022-01-26 07:31:20 +0000

    cmp pass by ref final

--- p5final.c
+++ p5final.c
@@ -1,39 +1,33 @@
 #include<stdio.h>
-void cmp(int a,int b,int c,int *large)
-{
-  if(a>b)
-  {
-    if(a>c)
-    {
-      *large=a;
-    }
-    else
-    {
-      *large=c;
-    }
-  }
-  else
-  {
-    if(b>c)
-    {
-      *large=b;
-    }
-    else
-    {
-      *large=c;
-    }
-  }
-}
 void input(int *a,int *b,int *c)
 {
-  printf("Enter the three numbers\n");
+  printf("enter the number:\n");
   scanf("%d %d %d",a,b,c);
+}
+int cmp(int a,int b,int c,int *large)
+{
+  if(a>b && a>c)
+  {
+    *large=a;
+  }  
+  else if(b>c)
+  {
+   *large=b;
+  } 
+  else
+  {
+   *large=c;
+  } 
+}
+void output(int large)
+{
+  printf("largest number is %d\n",large);
 }
 int main()
 {
-  int x,y,z,large;
-  input(&x,&y,&z);
-  cmp(x,y,z,&large);
-  printf("%d is the largest of them all\n",large);
+  int a,b,c,large;
+  input(&a,&b,&c);
+  cmp(a,b,c,&large);
+  output(large);
   return 0;
-}
+}  
